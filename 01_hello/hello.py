760de3696d2af94367fe04ca98ac09bb4fa1834b
Landmoncery <mnv9900@163.com>
2023-07-20 09:55:52 +0800

    230719 1.11

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -5,8 +5,9 @@ Purpose: Say hello
 '''
 
 import argparse
-
+# ----------------------------------------------
 def get_args():
+    '''Get the command-line arguments'''
     parser = argparse.ArgumentParser(description='Say hello')
     parser.add_argument('-n', '--name', metavar='name',
                         default='World', help='Name to greet')
@@ -14,9 +15,12 @@ def get_args():
 # note: be careful about the [space].
 # I was failed to pass the test just because the output is 'Hello,World!', missed the space
 
+# ----------------------------------------------
 def main():
-     args = get_args()
-     print('Hello, ' + args.name + '!')
-     
+    '''make a k-pop noise there'''
+    args = get_args()
+    print('Hello, ' + args.name + '!')
+
+# ----------------------------------------------     
 if __name__ == '__main__':
     main()
